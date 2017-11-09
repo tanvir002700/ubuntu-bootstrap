ab2cba666b3b3562ff475e7c0e932f88df584e64
tanvir hasan <tanvir002700@gmail.com>
2017-11-09 23:47:06 +0600

    add nodenv installation

---
+++ nodenv.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ -d ~/.nodenv ]; then
+  echo "=======> nodenv already installed <======="
+else
+    echo "------->> installing nodenv <<------"
+
+    git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+    pushd ~/.nodenv
+    pushd src/configure && make -C src
+    popd
+    popd
+    mkdir -p ~/.nodenv/plugins
+    pushd ~/.nodenv/plugins
+    git clone https://github.com/nodenv/node-build.git
+    git clone https://github.com/nodenv/nodenv-default-packages.git
+    popd
+    touch ~/.nodenv/default-packages
+fi
