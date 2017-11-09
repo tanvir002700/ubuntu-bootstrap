edf10fd866508fc36528a06869acc612c735fa75
tanvir hasan <tanvir002700@gmail.com>
2017-11-10 00:18:29 +0600

    add pyenv installation

---
+++ pyenv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -d ~/.pyenv ]; then
+    echo "=======> pyenv already installed <======="
+else
+    echo "------->> installing pyenv <<------"
+    
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    mkdir -p ~/.pyenv/plugins
+    pushd ~/.pyenv/plugins
+    git clone https://github.com/pyenv/pyenv-ccache.git
+    git clone https://github.com/pyenv/pyenv-version-ext.git
+    popd
+fi
