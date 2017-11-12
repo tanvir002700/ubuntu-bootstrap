16264c6fa928a0809b7ed02a96b2dd26c693cff4
tanvir hasan <tanvir002700@gmail.com>
2017-11-12 23:20:28 +0600

    add zsh intallation

---
+++ recipes/zsh.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+if which zsh > /dev/null; then
+  echo "=======> zsh already installed. <======="
+else
+  echo "------->> Installing zsh <<-------"
+  sudo apt-get install -y --allow zsh
+  chsh -s $(which zsh)
+fi
