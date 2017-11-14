d0241d78a1807dd1cff9e6b78cac8fd07aa0b820
tanvir hasan <tanvir002700@gmail.com>
2017-11-15 01:34:11 +0600

    add mysql installation

---
+++ recipes/mysql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if which mysql > dev/null; then
+	echo "=======> mysql already installed <======="
+else
+	echo "------->> installing mysql <<------"
+	sudo apt-get install mysql-server \
+						 mysql-client \
+						 libmysqlclient-dev -y --force-yes
+fi
