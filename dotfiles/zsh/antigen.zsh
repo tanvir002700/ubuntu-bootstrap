c1b11a0f1e31dbda97a60311206a3878bdce4b68
tanvir hasan <tanvir002700@gmail.com>
2017-11-16 00:18:49 +0600

    add antigen plugins

--- dotfiles/zsh/antigen.zsh
+++ dotfiles/zsh/antigen.zsh
@@ -5,5 +5,8 @@ antigen use oh-my-zsh
 antigen theme robbyrussell
 
 antigen bundle horosgrisa/autoenv
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
 
 antigen apply
